b8be9ff1c9d50a21da37399ff6809a39337b8658
BK <bramkelder@users.noreply.github.com>
2018-03-08 15:12:21 +0100

    Remove old file, fix sky color

--- Orbis/Orbis.cs
+++ Orbis/Orbis.cs
@@ -300,7 +300,7 @@ namespace Orbis
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Black);
+            GraphicsDevice.Clear(Color.Aqua);
 
             // TODO: Add your drawing code here
             //DrawHex();
