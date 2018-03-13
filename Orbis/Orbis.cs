672da04d48b4285c300c7a3cb5573ab9da6401f2
JannickZeegers <JannickZeegers@gmail.com>
2018-03-13 16:56:25 +0100

    Removed some testcode

--- Orbis/Orbis.cs
+++ Orbis/Orbis.cs
@@ -339,11 +339,6 @@ namespace Orbis
                 Exit();
             }
 
-            if (input.IsMouseDown(MouseButton.Left, new MouseButton[] { MouseButton.Right}))
-            {
-                distance += speed;
-            }
-
             angle = MathHelper.Clamp(angle, -80, -5);
             distance = MathHelper.Clamp(distance, 1, 4000);
 
