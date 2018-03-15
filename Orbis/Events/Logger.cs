89ebfa7b2cac6786937cc57896e39ac1d8c4d3e5
AukeM <AukeM@users.noreply.github.com>
2018-03-15 15:48:48 +0100

    Added logByType

--- Orbis/Events/Logger.cs
+++ Orbis/Events/Logger.cs
@@ -19,14 +19,26 @@ namespace Orbis.Events
 
         public void AddLog(string item, string type = DEFAULT_TYPE)
         {
-            Log logData = new Log(item, type);
-            log.Add(logData);
-            Debug.WriteLine(logData.ToString());
+            log.Add(new Log(item, type));
         }
 
         public List<Log> GetLog()
         {
             return log;
+        }
+
+        public List<Log> GetLogByType(string type)
+        {
+            List<Log> tempLog = new List<Log>();
+
+            foreach (var logItem in log)
+            {
+                if (logItem.Type == type)
+                {
+                    tempLog.Add(logItem);
+                }
+            }
+            return tempLog;
         }
     }
 }
