4be2a5a832582cd80bad653d2cbff30e19dda6ef
AukeM <AukeM@users.noreply.github.com>
2018-04-05 16:29:42 +0200

    Added random game music

--- Orbis/States/State/GameState.cs
+++ Orbis/States/State/GameState.cs
@@ -1,12 +1,36 @@
 ﻿using Orbis.Engine;
+using System;
 
 namespace Orbis.States.State
 {
     class GameState : IState
     {
+        private Random rand;
+
+        public GameState()
+        {
+            rand = new Random();
+        }
+
         public void Run()
         {
-            AudioManager.PlaySong("Crossing the Chasm");
+            PlayRandomSong();
+        }
+
+        /// <summary>
+        /// Get a random song
+        /// </summary>
+        private void PlayRandomSong()
+        {
+            int songRand = rand.Next(0, 2);
+            if (songRand == 0)
+            {
+                AudioManager.PlaySong("Crossing the Chasm");
+            }
+            else
+            {
+                AudioManager.PlaySong("Rocket");
+            }
         }
     }
 }
